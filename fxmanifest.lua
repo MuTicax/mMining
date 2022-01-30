dea8b70dd29bf694cd78c06c1214307ed2183361
MuTicax <muticax@gmail.com>
2022-01-30 03:57:01 +0100

    Update structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,11 +3,11 @@ game 'gta5'
 
 client_scripts {
 	'config.lua',
-    'client.lua'
+    'client/client.lua'
 }
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
     'config.lua',
-	'server.lua'
+	'server/server.lua'
 }
